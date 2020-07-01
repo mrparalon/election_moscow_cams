b9cbb6ca3364e52b532c5981831695abae381acd
mrparalon <mr.paralon@gmail.com>
2020-07-01 09:53:38 +0300

    Add time renaming

--- rename.py
+++ rename.py
@@ -19,6 +19,14 @@ for station_name in station_names:
     station_path = path / station_name
     for cam in range(2):
         cam_path = station_path / str(cam+1)
+        files = os.listdir(cam_path)
+        for file_name in files:
+            file_path = cam_path / file_name
+            new_name = file_name.split('.')[0]
+            new_name = new_name.replace('_', '-')
+            new_name = new_name.replace('-', '_', 2)
+            new_name = f'{new_name}.png'
+            file_path.rename(cam_path / new_name)
         station_obj = cam_ids.get(station_name)
         if station_obj is not None:
             cam_uuid = station_obj['cameras'][cam]['uuid']
